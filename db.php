e2a0b94875222c0b21fbf6af3a0e3bba6f84e302
yva23 <96648111+yva23@users.noreply.github.com>
2022-01-04 16:50:43 +0300

    Update db.php

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 // Enter your Host, username, password, database below.
 // I left password empty because i do not set password on localhost.
 $con = mysqli_connect("localhost","root","bakha_23","register");
-// Check connection
+// Check connection 
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
